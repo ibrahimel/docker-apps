0a59c8bbef56ede8013a27a7639d86133c257018
Ibrahim El Rhezzali <ibrahim.el@pm.me>
2019-06-12 15:11:39 +0000

    Error

---
+++ fix-files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source "./spinner.sh"
+echo '[Docker-apps] -- Preparing to Fix packages'
+
+cd build/
+
+# Fix files
+list=$(ls)
+
+for package in $list; do
+	if [ -d $package ]; then
+		start_spinner "[Docker-apps] -- Fixing package: $package" && echo 'Starting'
+
+		sed -i 's@apt-get@apt@' ./$package/Dockerfile && echo 'Dockerfile APT Done'
+		sed -i 's@apt-get@apt@' ./$package/nvidia.Dockerfile && echo 'nvidia Dockerfile APT Done'
+
+		PNG=$(cat ./$package/build.sh | grep 'docker cp' | grep '.png')
+		if [ -n "$PNG" ]; then
+			rm -r ./$package/*.png && echo 'PNG Clean Done'
+		fi
+
+		ICO=$(cat ./$package/build.sh | grep 'docker cp' | grep '.ico')
+		if [ -n "$ICO" ]; then
+			rm -r ./$package/*.ico && echo 'ICO Clean Done'
+		fi
+
+		SVG=$(cat ./$package/build.sh | grep 'docker cp' | grep '.svg')
+		if [ -n "$SVG" ]; then
+			rm -r ./$package/*.svg && echo 'SVG Clean Done'
+		fi
+
+		XPM=$(cat ./$package/build.sh | grep 'docker cp' | grep '.xpm')
+		if [ -n "$XPM" ]; then
+			rm -r ./$package/*.xpm && echo 'XPM Clean Done'
+		fi
+
+		DESK=$(cat ./$package/build.sh | grep 'docker cp' | grep '.desktop')
+		if [ -n "$DESK" ]; then
+			rm -r ./$package/*.desktop && echo 'DESK Clean Done'
+		fi
+
+		stop_spinner $? && echo 'Done'
+	fi
+done
+
+rm -rf ./vpn-router/ovpn/*
